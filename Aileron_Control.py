3504007dc92be4203235ed8b42b44b437fcd54a7
SinanKamil <kamil_sinan@yahoo.com>
2023-03-13 20:25:08 -0700

    new aileron file

--- Aileron_Control.py
+++ Aileron_Control.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-Enable = 23
+Enable = 24
 Speed = 18 #PWM 0 pin
-F_R = 24 # 1 is forward and 0 is reverse
+F_R = 16 # 1 is forward and 0 is reverse(UP)
 Break = 3
 
 GPIO.setwarnings(False)
@@ -15,16 +15,19 @@ GPIO.setup(Speed, GPIO.OUT)
 GPIO.setup(Break, GPIO.OUT)
 
 GPIO.output(Enable, 0)
-GPIO.output(F_R, 1)
+
 GPIO.output(Speed, 1)
 GPIO.output(Break, 1)
 p = GPIO.PWM(Speed,2000)   # Initialize PWM on pin 12 with a frequency of 50Hz
 p.start(0)
 
-for duty in range(0,100,1):
-    p.ChangeDutyCycle(duty)
-    sleep(.02)
-sleep(.01)
+#for duty in range(0,100,5):
+#    p.ChangeDutyCycle(duty)
+while True:
+    GPIO.output(F_R, 0)
+    p.ChangeDutyCycle(5)
+
+
 
     
     
