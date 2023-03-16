64a9aa8915ef4e78012e6ef0febc1773d620f8c1
SinanKamil <kamil_sinan@yahoo.com>
2023-03-15 17:44:31 -0700

    changed the alternator and added new files

--- Aileron_test_copy_Jacob_Chris.py
+++ Aileron_test_copy_Jacob_Chris.py
@@ -1,20 +1,20 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-Enable = 24
-Speed = 18 #PWM 0 pin
-F_R = 16 # 1 is forward and 0 is reverse(UP)
+Enable = 15 #by default is high 
+Speed = 21 #PWM 0 pin
+F_R = 7 # 1 is forward and 0 is reverse(UP)
 Break = 3
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)#look at GPIO
 
 GPIO.setup(Enable, GPIO.OUT)
 GPIO.setup(F_R, GPIO.OUT)
 GPIO.setup(Speed, GPIO.OUT)
 GPIO.setup(Break, GPIO.OUT)
 
-GPIO.output(Enable, 0)
+GPIO.output(Enable, 1)
 GPIO.output(F_R, 1)
 GPIO.output(Speed, 1)
 GPIO.output(Break, 1)
@@ -27,6 +27,6 @@ while True:
     p.ChangeDutyCycle(5)
 
 
-
+GPIO.cleanup()
     
     
