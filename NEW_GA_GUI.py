e0f91c14c5ffceca7e3ac81265d5feae01ebd07d
SinanKamil <kamil_sinan@yahoo.com>
2023-03-28 17:36:10 -0700

    figured out the aliternator

--- NEW_GA_GUI.py
+++ NEW_GA_GUI.py
@@ -15,15 +15,7 @@ import threading
 #from centering_steering import center
 #from admin_antenna import left_antenna, right_antenna, disable_antenna
 #from user_steering import user_steering_run
-
-#import RPi.GPIO as GPIO
-
-#GPIO.setwarnings(False)
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(10, GPIO.OUT)
-#pwm = GPIO.PWM(10, 100)
-#pwm.start(0)
-
+from user_LEDlights_DC import slider_function
 
 
 current_value = 0
@@ -265,7 +257,7 @@ class GA(tk.Tk):
 
 
 
-        self.w1 = Scale(self.alternator_page, from_=0, to=100, length=1000, orient=HORIZONTAL,
+        self.w1 = Scale(self.alternator_page, from_=0, to=100, length=1000, orient=HORIZONTAL, command=slider_function(current_value),
                         troughcolor='#0e3999', width=67, sliderrelief='groove', highlightbackground='#0e3999',
                         sliderlength=40, font= ("Tactic Sans Extra Extended", 15), showvalue=0)
         self.w1.set(current_value)
--- user_LEDlights_DC.py
+++ user_LEDlights_DC.py
@@ -1,35 +1,28 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-def DC_LED_function():
+def init_Alternator():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(9, GPIO.OUT)#for DC motor
     GPIO.setup(10, GPIO.OUT)#for LED
 
 
+    
+    
+def slider_function(slider_value):
+    init_Alternator()
     p = GPIO.PWM(10,100)   # Initialize PWM on pin 12 with a frequency of 50Hz
     pwmDC = GPIO.PWM(9, 100)
     p.start(0)              # Start the PWM with a duty cycle of 0
     pwmDC.start(0)
+    while True:
+        p.ChangeDutyCycle(slider_value)
+        pwmDC.ChangeDutyCycle(slider_value)
 
-    times = 3
-    for i in range(times):
-        for duty in range(0,100,1):
-            p.ChangeDutyCycle(duty)
-            pwmDC.ChangeDutyCycle(duty)
-
-            sleep(.02)
-        sleep(.01)
-        
-        for duty in range(100,-1,-1):
-            p.ChangeDutyCycle(duty)
-            pwmDC.ChangeDutyCycle(duty)
-
-            sleep(.02)
-        sleep(.01)
+        sleep(.02)
+    sleep(.01)
     GPIO.cleanup()
-
 
 
 
