c0c7b471afc7555613e65c3993cab40f4429d8a8
SinanKamil <kamil_sinan@yahoo.com>
2023-03-31 17:57:39 -0700

    added uart to work on both retract and steering

--- two_UARTS_pi4_get.py
+++ two_UARTS_pi4_get.py
@@ -3,17 +3,28 @@ import RPi.GPIO as GPIO
 import smbus
 bus = smbus.SMBus(1)
 
-def validate_data(serial_obj):
-    while True:
+def retract_validate_data(serial_obj):
         try:
-            data = serial_obj.readline(4)
-            data_float = float(data)
-            if 0.00 <= data_float <= 2.00:
-                data_float = "{:.2f}".format(data_float)
-                return data_float
+            data = serial_obj.readline(9).decode('utf-8')
+            data_str = str(data)
+            split_float = data_str.split(" ")
+            data_float_retract = float(split_float[1])
+            if 0 <= data_float_retract <= 2.00:
+                data_float_retract = "{:.2f}".format(data_float_retract)
+            return float(data_float_retract)
         except ValueError:
             pass
-
+def steering_validate_data(serial_obj):
+        try:
+            data = serial_obj.readline(9).decode('utf-8')
+            data_str = str(data)
+            split_float = data_str.split(" ")
+            data_float_steering = float(split_float[0])
+            if 0 <= data_float_steering <= 2.00:
+                data_float_steering = "{:.2f}".format(data_float_steering)
+            return float(data_float_steering)
+        except ValueError:
+            pass
 def i2c_validate_data():
     try:
         aileron_num = bus.read_byte(0x41)
@@ -27,10 +38,12 @@ def i2c_validate_data():
 
 ser = serial.Serial("/dev/ttyS0", 115200)
 
+
 while True:
-    steering = validate_data(ser)
+    steering = steering_validate_data(ser)
+    retract = retract_validate_data(ser)
     aileron = i2c_validate_data()
-    if aileron is not None:
-        print("Steering: ", steering, "Aileron: ", aileron)
+    #if aileron is not None:
+    print("Steering: ", steering, "Retract: ", retract ,"Aileron: ", aileron)
 
 ser.close()
