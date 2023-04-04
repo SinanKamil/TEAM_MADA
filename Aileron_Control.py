84b5cb0cf7dd92fddb423741945a8d4628da7223
SinanKamil <kamil_sinan@yahoo.com>
2023-04-03 17:24:01 -0700

    Aileron program

--- Aileron_Control.py
+++ Aileron_Control.py
@@ -6,33 +6,38 @@ Speed = 21 #PWM 0 pin
 F_R = 7 # 1 is forward and 0 is reverse(UP)
 Break = 3
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)#look at GPIO
+Aileron_max_speed = 50
 
-GPIO.setup(Enable, GPIO.OUT)
-GPIO.setup(F_R, GPIO.OUT)
-GPIO.setup(Speed, GPIO.OUT)
-GPIO.setup(Break, GPIO.OUT)
-
-GPIO.output(Enable, 1)
-GPIO.output(Speed, 1)
-GPIO.output(Break, 1)
-p = GPIO.PWM(Speed,2000)   # Initialize PWM on pin 12 with a frequency of 50Hz
-p.start(0)
-
-#for duty in range(0,100,5):
-#    p.ChangeDutyCycle(duty)
-while True:
-    GPIO.output(F_R, 1)
-    p.ChangeDutyCycle(50)
-    sleep(1)
-    GPIO.output(F_R, 0)
-    p.ChangeDutyCycle(50)
-    sleep(1)
-    break
-
-
-GPIO.cleanup()
     
+def aileron_init(F_R_val):
+
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)#look at GPIO
+
+    GPIO.setup(Enable, GPIO.OUT)
+    GPIO.setup(F_R, GPIO.OUT)
+    GPIO.setup(Speed, GPIO.OUT)
+    GPIO.setup(Break, GPIO.OUT)
+
+    GPIO.output(Enable, 1)
+    GPIO.output(Speed, 1)
+    GPIO.output(Break, 1)
+    GPIO.output(F_R, F_R_val)
+
+
+def aileron_control():   
+    while True:
+        aileron_init(1)
+        p = GPIO.PWM(Speed,2000)   # Initialize PWM on pin 12 with a frequency of 50Hz
+        p.start(0)
+        p.ChangeDutyCycle(10)
+        sleep(5)
+        aileron_init(0)
+        p.ChangeDutyCycle(10)
+        sleep(5)
+        break
+    GPIO.cleanup()
+    
+
     
 
---
+++ aileron_center.py
@@ -0,0 +1,7 @@
+import RPi.GPIO as GPIO
+from time import sleep
+from Aileron_Control import aileron_control, Aileron_max_speed
+
+print (Aileron_max_speed)
+
+aileron_control()
