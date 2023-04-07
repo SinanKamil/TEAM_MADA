52e6faf2036292e0293cdcc0aef32f4a93a30db1
SinanKamil <kamil_sinan@yahoo.com>
2023-04-06 19:13:42 -0700

    Added pico code

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
-import tkinter as tk
+import machine
+import os
+from machine import Pin, ADC
+import utime
+from machine import mem32,Pin
+from i2cSlave import i2c_slave
 
-class ExampleApp(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
 
-    def create_widgets(self):
-        # Create the button image
-        self.switch_button_img = tk.PhotoImage(file="btn_images/Alternator.png")
+led_pin = machine.Pin(25, machine.Pin.OUT)
 
-        # Create the button widget
-        self.switch_button = tk.Button(self, image=self.switch_button_img, command=self.toggle_switch)
-        self.switch_button.pack()
 
-    def toggle_switch(self):
-        # Toggle the button state between normal and disabled
-        if self.switch_button.config('state')[-1] == "normal":
-            self.switch_button.config(state="disabled")
-            print("OFFFF")
-        else:
-            self.switch_button.config(state="normal")
+uart = machine.UART(1, 115200)#steering
 
-root = tk.Tk()
-app = ExampleApp(master=root)
-app.mainloop()
+
+POT_Value_steer = ADC(26)
+POT_Value_retract = ADC(27)
+POT_Value_aileron = ADC(28)
+convert_factor = 2/(65536)#for voltage convertion
+
+
+while True:
+    led_pin.value(1)
+    
+    out_aileron = POT_Value_aileron.read_u16() * convert_factor
+    out_round_aileron = "{:,.2f}".format(out_aileron)  
+    
+    out_steer = POT_Value_steer.read_u16() * convert_factor
+    out_round_steer = "{:,.2f}".format(out_steer)
+    
+    out_retract = POT_Value_retract.read_u16() * convert_factor
+    out_round_retract = "{:,.2f}".format(out_retract)
+
+    space = " "
+    out_ster_retr = str(out_round_steer) + space + str(out_round_retract) + space + str(out_round_aileron)
+    uart.write(out_ster_retr)
+    
+        
+    
+    
+    
+    
+    print("Steering:", str(out_round_steer), " ", "Retract:",  str(out_round_retract), "Aileron:", out_round_aileron)
+    utime.sleep(0.1)
