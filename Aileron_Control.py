68abbb1d4fe3a76fdea7213e15d1307ca2944ae4
Sinan Kamil <44329818+SinanKamil@users.noreply.github.com>
2023-04-07 18:56:04 -0700

    Update Aileron_Control.py

--- Aileron_Control.py
+++ Aileron_Control.py
@@ -9,44 +9,95 @@ Break = 3
 Aileron_max_speed = 5
 
 
+def setup():
+    try:
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)#look at GPIO
+
+        GPIO.setup(Enable, GPIO.OUT)
+        GPIO.setup(F_R, GPIO.OUT)
+        GPIO.setup(Speed, GPIO.OUT)
+        GPIO.setup(Break, GPIO.OUT)
+
+    except Exception as e:
+        print(f"Error setting up GPIO pins: {e}")
+        GPIO.cleanup()
+        exit()
+
 def aileron_init(F_R_val, EN, Break_val):
+    try:
+        GPIO.output(Enable, EN)
+        GPIO.output(Speed, 1)
+        GPIO.output(Break, Break_val)
+        GPIO.output(F_R, F_R_val)
+        p = GPIO.PWM(Speed, 2000)  # Initialize PWM on pin 12 with a frequency of 50Hz
+        p.start(0)
+        return p
 
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)#look at GPIO
-
-    GPIO.setup(Enable, GPIO.OUT)
-    GPIO.setup(F_R, GPIO.OUT)
-    GPIO.setup(Speed, GPIO.OUT)
-    GPIO.setup(Break, GPIO.OUT)
-
-    GPIO.output(Enable, EN)
-    GPIO.output(Speed, 1)
-    GPIO.output(Break, Break_val)
-    GPIO.output(F_R, F_R_val)
-
-
-def aileron_forward():
-    aileron_init(0, 1, 1)
-    p = GPIO.PWM(Speed, 2000)  # Initialize PWM on pin 12 with a frequency of 50Hz
-    p.start(0)
-    p.ChangeDutyCycle(10)
+    except Exception as e:
+        print(f"Error initializing aileron: {e}")
+        GPIO.cleanup()
+        exit()
 
 
-def aileron_reverse():
-    aileron_init(1,1,1)
-    p = GPIO.PWM(Speed, 2000)
-    p.ChangeDutyCycle(10)
+def aileron_forward(p):
+    try:
+        aileron_init(0, 1, 1)
+        p.ChangeDutyCycle(10)
+
+    except Exception as e:
+        print(f"Error moving aileron forward: {e}")
+        GPIO.cleanup()
+        exit()
+
+
+def aileron_reverse(p):
+    try:
+        aileron_init(1,1,1)
+        p.ChangeDutyCycle(10)
+
+    except Exception as e:
+        print(f"Error moving aileron reverse: {e}")
+        GPIO.cleanup()
+        exit()
 
 
 def aileron_disable():
-    GPIO.output(Enable, 0)
-    GPIO.output(Break, 0)
+    try:
+        GPIO.output(Enable, 0)
+        GPIO.output(Break, 0)
+
+    except Exception as e:
+        print(f"Error disabling aileron: {e}")
+        GPIO.cleanup()
+        exit()
 
 
-while True:
-    aileron_reverse()
-    sleep(1)
-    aileron_forward()
-    sleep(1)
-    aileron_disable()
-    break
+def cleanup():
+    try:
+        GPIO.cleanup()
+        print("GPIO pins cleaned up successfully.")
+
+    except Exception as e:
+        print(f"Error cleaning up GPIO pins: {e}")
+        exit()
+
+
+if __name__ == '__main__':
+    setup()
+    p = aileron_init(0, 1, 1)
+
+    while True:
+        try:
+            aileron_reverse(p)
+            sleep(1)
+            aileron_forward(p)
+            sleep(1)
+            aileron_disable()
+            sleep(1)
+
+        except KeyboardInterrupt:
+            print("Exiting script due to keyboard interrupt.")
+            break
+
+    cleanup()
