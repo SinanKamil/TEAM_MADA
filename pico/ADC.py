3ba35a56a42bb006cb57d8de1fdc3146dec1b614
Sinan Kamil <44329818+SinanKamil@users.noreply.github.com>
2023-04-10 23:16:29 -0700

    Delete pico directory

--- pico/ADC.py
+++
@@ -1,8 +0,0 @@
-from machine import Pin, ADC
-import utime
-
-POT_Value = ADC(26)
-convert_factor = 12/ (65536)#for voltage convertion
-while True:
-    print(POT_Value.read_u16() * convert_factor,"volts")
-    utime.sleep(0.1)
