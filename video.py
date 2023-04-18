14c396fdafd7221efacff9a0a7ee398bd6b38e65
Sinan Kamil <kamil_sinan@yahoo.com>
2023-04-18 11:49:56 -0700

    video

--- video.py
+++ video.py
@@ -1,29 +1,58 @@
 import cv2
 
-def play_video_vlc(video_file_path):
-    # Define the command to launch VLC with the video file
-    vlc_cmd = ["vlc", "-f", video_file_path]
 
-    # Launch VLC with the video file
-    vlc_process = subprocess.Popen(vlc_cmd)
+def play_video():
+    # Create a VideoCapture object and read from input file
+    cap = cv2.VideoCapture("C:/Users/kamil/OneDrive/Main Files/GA_SLIDESHOW/1906_SkyGuardian_UK_Beauty_Reel_060719.mp4")
 
-    # Define the mouse/touch event callback function
+    # Reduce the resolution to 720p
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    # Reduce the frame rate to 30 fps
+    cap.set(cv2.CAP_PROP_FPS, 30)
+
+    # Check if camera opened successfully
+    if not cap.isOpened():
+        print("Error opening video file")
+
+    # Create a window with full screen size
+    cv2.namedWindow('Frame', cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty('Frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    # Function to handle mouse click events
     def mouse_callback(event, x, y, flags, param):
-        # Exit video playback if right mouse button is pressed or touch event is detected
-        if event == cv2.EVENT_RBUTTONDOWN or event == cv2.EVENT_LBUTTONDOWN:
-            vlc_process.terminate()
+        # Exit video playback if right mouse button is pressed on video window
+        if event == cv2.EVENT_RBUTTONDOWN:
+            cap.release()
 
-    # Register the mouse/touch event callback function
-    cv2.namedWindow('Video Player', cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty('Video Player', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    cv2.setMouseCallback('Video Player', mouse_callback)
+    # Register mouse click event callback
+    cv2.setMouseCallback('Frame', mouse_callback)
 
-    # Wait for VLC to finish playing the video
-    vlc_process.wait()
+    # Read until video is completed
+    while cap.isOpened():
 
-    # Clean up OpenCV windows
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret:
+            # Display the resulting frame
+            cv2.imshow('Frame', frame)
+
+            # Press Q or X on keyboard to exit
+            key = cv2.waitKey(25)
+            if key & 0xFF == ord('q') or key == 27 or key == ord('x'):
+                break
+
+        # If the video is finished, set the position to the beginning
+        else:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    # When everything done, release
+    # the video capture object
+    cap.release()
+
+    # Closes all the frames
     cv2.destroyAllWindows()
 
-# Call the function with the path to the video file as an argument
-video_file_path = "/path/to/video/file.mp4"
-play_video_vlc(video_file_path)
+
+play_video()
