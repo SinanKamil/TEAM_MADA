9142a7548c75efda35dfc70de754902c419a6974
Sinan Kamil <kamil_sinan@yahoo.com>
2023-04-25 12:20:41 -0700

    retract

--- user_directional_antenna.py
+++ user_directional_antenna.py
@@ -5,89 +5,93 @@ def antenna():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
 
-    GPIO.setup(27, GPIO.OUT) #left27
-    GPIO.setup(18, GPIO.OUT) #right18
-    GPIO.setup(17, GPIO.OUT) #enable pin
+    left = 27
+    right = 18
+    enable = 17
 
-    # Rotate the motor clockwise
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.LOW)
+    GPIO.setup(left, GPIO.OUT) #left27
+    GPIO.setup(right, GPIO.OUT) #right18
+    GPIO.setup(enable, GPIO.OUT) #enable pin
 
     # Turn on the motor by setting the enable pin to high
-    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(enable, GPIO.HIGH)
+
+    # Rotate the motor clockwise
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.LOW)
 
     # Wait for 1 seconds
     time.sleep(1)
 
     # Pause the motor for 0.5
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.HIGH)
     time.sleep(0.5)
 
     # Rotate the motor counterclockwise
-    GPIO.output(27, GPIO.LOW)
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(left, GPIO.LOW)
+    GPIO.output(right, GPIO.HIGH)
     time.sleep(3)
 
     # Pause the motor for 0.5
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.HIGH)
     time.sleep(0.5)
 
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.LOW)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.LOW)
     time.sleep(5)
 
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.HIGH)
     time.sleep(0.5)
 
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.LOW)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.LOW)
     time.sleep(0.5)
 
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.HIGH)
     time.sleep(0.5)
 
-    GPIO.output(27, GPIO.LOW)
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(left, GPIO.LOW)
+    GPIO.output(right, GPIO.HIGH)
     time.sleep(4)
 
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.HIGH)
     time.sleep(0.5)
 
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.LOW)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.LOW)
     time.sleep(0.5)
 
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.HIGH)
     time.sleep(0.5)
 
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.LOW)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.LOW)
     time.sleep(0.5)
 
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.HIGH)
     time.sleep(0.5)
 
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.LOW)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.LOW)
     time.sleep(0.5)
 
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(left, GPIO.HIGH)
+    GPIO.output(right, GPIO.HIGH)
     time.sleep(0.2)
 
-    GPIO.output(27, GPIO.LOW)
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(left, GPIO.LOW)
+    GPIO.output(right, GPIO.HIGH)
     time.sleep(1.5)
 
     # Turn off the motor by setting the enable pin to low
-    GPIO.output(17, GPIO.LOW)
+    GPIO.output(enable, GPIO.LOW)
     
 
 # Clean up the GPIO pins
